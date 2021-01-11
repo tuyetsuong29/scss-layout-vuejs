558b53dd2140ea0f277ba65b714fba79c8abde03
suong nguyen <suongntt@cozwork.com>
2021-01-11 15:52:11 +0700

    chia scss

---
+++ src/components/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="bodyWrapper">
+    <div class="sidebarWrapper">
+      <div class="nameLink">
+        <p>Home</p>
+        <p>Home</p>
+        <p>Home</p>
+        <p>Home</p>
+        <p>Home</p>
+      </div>
+    </div>
+    <div class="container">
+      <div class="headerWrraper">
+        Home.something
+      </div>
+      <div class="bodyWrraper">
+        <div class="cardBody">
+          write something here...
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  components: {},
+};
+</script>
+<style lang="scss">
+@import "../assets/home/home.scss";
+</style>
